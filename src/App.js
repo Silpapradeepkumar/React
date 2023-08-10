b0956f9bf27a6b3d43fda4d093b5a8833fd21c78
Silpa <anvitashilpa@gmail.com>
2023-08-10 10:18:33 +0530

    added heading

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Button from 'react-bootstrap/Button';
 function App() {
   return (
     <div>
+      <h1>welcome</h1>
       <New class="class1"  head= "The Seasons Latest" para = "Get the seasons all latest designs in a flick of your hand "/>
       
       <New class="class2" head= "Our New Designs" para = "Get the designs,developed by our in-house team all for yourself"/>
